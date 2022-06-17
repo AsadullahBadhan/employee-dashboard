0a79c4f09deb619fcc37c87c8cfb9ddf373a97b6
AsadullahBadhan <asadullah.badhan@gmail.com>
2022-06-18 04:56:49 +0600

    some small issue fixed and remove console log

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -4,17 +4,15 @@ import './Summary.css'
 const Summary = (props) => {
   const totalEmployee = props.employees;
   const paidEmployee = props.employee;
-  const totalPaid = paidEmployee.length;
   let balance = totalEmployee.reduce((total, employee) => employee.salary + total, 0);
   const totalAmount = paidEmployee.reduce((total, employee) => employee.salary + total, 0);
   balance = balance - totalAmount;
-  console.log(totalEmployee)
 
   return (
     <div className='summary'>
       <div className='summary-card'>
         <h5>Salary Paid</h5>
-        <h4>{totalPaid}/{totalEmployee.length}</h4>
+        <h4>{paidEmployee.length}/{totalEmployee.length}</h4>
       </div>
       <div className='summary-card'>
         <h5>Total Amount Paid</h5>
